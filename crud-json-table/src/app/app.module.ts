83b8bf18c92c75c81806359931d49240adc1ee6d
kmvwork <kmvwork>
2021-11-21 17:59:55 +0300

    Add Error Page structure.

--- crud-json-table/src/app/app.module.ts
+++ crud-json-table/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { ErrorPageComponent } from './components/error-page/error-page.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
